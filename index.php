46f5cfb45c13d2dda0fe4bc088f2d6ab2b652d57
Victor Oka <oka.victor97@gmail.com>
2015-10-30 14:59:24 -0200

    1.6
    index pronto

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
     
 <section> 
     <article class="textoAtor">
-        <p>O site fala sobre blablabla.....</p>
+        <p>O site fala um pouco sobre "Eduardo e Monica", uma canção composta por Renato Russo e lançada em 1986, no álbum Dois, do grupo Legião Urbana, que foi editada como o segundo single do álbum no mesmo ano.</p>
+        <p>A canção, talvez uma das mais famosas da banda, narra, de forma linear, em quase cinco minutos, a história de amor de duas pessoas muito diferentes entre si e sua letra é composta de frases bem-humoradas e humorísticas que descrevem as personagens ao mesmo tempo em que mostram a evolução de sua relação.</p>
+        <p>Este site foi desenvolvido pelos alunos Lucas Portella e Victor Oka da Universidade Presbiteriana Mackenzie em 2015.</p>
     </article>
     <article class="textoPersonagem">
         <h2>Login</h2>

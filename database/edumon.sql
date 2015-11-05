046979ba2adf39c10b805a365f23065afb651cb4
luucasportella <lucas_portellasantos@yahoo.com.br>
2015-11-05 14:37:48 -0200

    1.10
    Novo banco de dados

--- database/edumon.sql
+++ database/edumon.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 22-Out-2015 às 20:52
+-- Generation Time: 05-Nov-2015 às 17:36
 -- Versão do servidor: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -29,20 +29,19 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `bd` (
   `nome` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `senha` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `cidade` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  `estado` varchar(2) COLLATE utf8_unicode_ci NOT NULL
+  `estado` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `foto` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `bd`
 --
 
-INSERT INTO `bd` (`nome`, `email`, `senha`, `cidade`, `estado`) VALUES
-('Lucas Teste', 'teste@teste', 'teste', 'Teste', 'AC'),
-('Lucas Portella', 'lucas@yahoo', 'asdfd', 'SP', 'Ja'),
-('Victor Oka', 'victor@email', 'senha', 'SP', 'SP'),
-('Lucas P S', 'aquele@passinho', 'aquele', 'Andre Marques', 'DJ');
+INSERT INTO `bd` (`nome`, `email`, `senha`, `cidade`, `estado`, `foto`) VALUES
+('Lucas Portella', 'lucas@yahoo', '698dc19d489c4e4db73e28a713eab07b', 'SÃ£o Paulo', 'SP', ''),
+('Jesus Cristo', 'lucas@sim', '110d46fcd978c24f306cd7fa23464d73', 'CÃ©u', 'CE', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
